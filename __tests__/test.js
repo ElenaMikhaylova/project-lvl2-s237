00f3d3e18efa2a048ca1bc6ecfc01d460e75bee9
Elena Mikhaylova <elena_tuzkova@yahoo.com>
2019-09-28 20:55:37 +0300

    fix tests

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,112 +1,92 @@
 import fs from 'fs';
+import path from 'path';
 import genDiff from '../src';
 
-const pathToTestDir = '__tests__/__fixtures__/';
+const fixturesPath = path.join(__dirname, '__fixtures__');
+const prettyExpected = fs.readFileSync(path.join(fixturesPath, 'diff1'), 'UTF-8');
+const plainExpected = fs.readFileSync(path.join(fixturesPath, 'diff1_plain'), 'UTF-8');
+const JSONExpected = fs.readFileSync(path.join(fixturesPath, 'diff1_json'), 'UTF-8');
+const prettyTreeExpected = fs.readFileSync(path.join(fixturesPath, 'diff2'), 'UTF-8');
+const plainTreeExpected = fs.readFileSync(path.join(fixturesPath, 'diff2_plain'), 'UTF-8');
 
 describe('Json', () => {
-  const pathToFile1 = `${pathToTestDir}before1.json`;
-  const pathToFile2 = `${pathToTestDir}after1.json`;
-  const pathToResult1 = `${pathToTestDir}diff1`;
-  const pathToResult2 = `${pathToTestDir}diff1_plain`;
-  const pathToResult3 = `${pathToTestDir}diff1_json`;
-  const result1 = fs.readFileSync(pathToResult1, 'utf-8');
-  const result2 = fs.readFileSync(pathToResult2, 'utf-8');
-  const result3 = fs.readFileSync(pathToResult3, 'utf-8');
+  const pathToFile1 = path.join(fixturesPath, 'before1.json');
+  const pathToFile2 = path.join(fixturesPath, 'after1.json');
   it('#main format', () => {
     const diff = `${genDiff(pathToFile1, pathToFile2)}\n`;
-    expect(diff).toBe(result1);
+    expect(diff).toBe(prettyExpected);
   });
   it('#plain format', () => {
     const diff = genDiff(pathToFile1, pathToFile2, 'plain');
-    expect(diff).toBe(result2);
+    expect(diff).toBe(plainExpected);
   });
   it('#json format', () => {
     const diff = `${genDiff(pathToFile1, pathToFile2, 'json')}\n`;
-    expect(diff).toBe(result3);
+    expect(diff).toBe(JSONExpected);
   });
 });
 
 describe('Json tree', () => {
-  const pathToFile1 = `${pathToTestDir}before2.json`;
-  const pathToFile2 = `${pathToTestDir}after2.json`;
-  const pathToResult1 = `${pathToTestDir}diff2`;
-  const pathToResult2 = `${pathToTestDir}diff2_plain`;
-  const result1 = fs.readFileSync(pathToResult1, 'utf-8');
-  const result2 = fs.readFileSync(pathToResult2, 'utf-8');
+  const pathToFile1 = path.join(fixturesPath, 'before2.json');
+  const pathToFile2 = path.join(fixturesPath, 'after2.json');
   it('#main format', () => {
     const diff = `${genDiff(pathToFile1, pathToFile2)}\n`;
-    expect(diff).toBe(result1);
+    expect(diff).toBe(prettyTreeExpected);
   });
   it('#plain format', () => {
     const diff = genDiff(pathToFile1, pathToFile2, 'plain');
-    expect(diff).toBe(result2);
+    expect(diff).toBe(plainTreeExpected);
   });
 });
 
 describe('Yaml', () => {
-  const pathToFile1 = `${pathToTestDir}before1.yaml`;
-  const pathToFile2 = `${pathToTestDir}after1.yaml`;
-  const pathToResult1 = `${pathToTestDir}diff1`;
-  const pathToResult2 = `${pathToTestDir}diff1_plain`;
-  const result1 = fs.readFileSync(pathToResult1, 'utf-8');
-  const result2 = fs.readFileSync(pathToResult2, 'utf-8');
+  const pathToFile1 = path.join(fixturesPath, 'before1.yaml');
+  const pathToFile2 = path.join(fixturesPath, 'after1.yaml');
   it('#main format', () => {
     const diff = `${genDiff(pathToFile1, pathToFile2)}\n`;
-    expect(diff).toBe(result1);
+    expect(diff).toBe(prettyExpected);
   });
   it('#plain format', () => {
     const diff = genDiff(pathToFile1, pathToFile2, 'plain');
-    expect(diff).toBe(result2);
+    expect(diff).toBe(plainExpected);
   });
 });
 
 describe('Yaml tree', () => {
-  const pathToFile1 = `${pathToTestDir}before2.yaml`;
-  const pathToFile2 = `${pathToTestDir}after2.yaml`;
-  const pathToResult1 = `${pathToTestDir}diff2`;
-  const pathToResult2 = `${pathToTestDir}diff2_plain`;
-  const result1 = fs.readFileSync(pathToResult1, 'utf-8');
-  const result2 = fs.readFileSync(pathToResult2, 'utf-8');
+  const pathToFile1 = path.join(fixturesPath, 'before2.yaml');
+  const pathToFile2 = path.join(fixturesPath, 'after2.yaml');
   it('#main format', () => {
     const diff = `${genDiff(pathToFile1, pathToFile2)}\n`;
-    expect(diff).toBe(result1);
+    expect(diff).toBe(prettyTreeExpected);
   });
   it('#plain format', () => {
     const diff = genDiff(pathToFile1, pathToFile2, 'plain');
-    expect(diff).toBe(result2);
+    expect(diff).toBe(plainTreeExpected);
   });
 });
 
 describe('Ini', () => {
-  const pathToFile1 = `${pathToTestDir}before1.ini`;
-  const pathToFile2 = `${pathToTestDir}after1.ini`;
-  const pathToResult1 = `${pathToTestDir}diff1`;
-  const pathToResult2 = `${pathToTestDir}diff1_plain`;
-  const result1 = fs.readFileSync(pathToResult1, 'utf-8');
-  const result2 = fs.readFileSync(pathToResult2, 'utf-8');
+  const pathToFile1 = path.join(fixturesPath, 'before1.ini');
+  const pathToFile2 = path.join(fixturesPath, 'after1.ini');
   it('#main format', () => {
     const diff = `${genDiff(pathToFile1, pathToFile2)}\n`;
-    expect(diff).toBe(result1);
+    expect(diff).toBe(prettyExpected);
   });
   it('#plain format', () => {
     const diff = genDiff(pathToFile1, pathToFile2, 'plain');
-    expect(diff).toBe(result2);
+    expect(diff).toBe(plainExpected);
   });
 });
 
 describe('Ini tree', () => {
-  const pathToFile1 = `${pathToTestDir}before2.ini`;
-  const pathToFile2 = `${pathToTestDir}after2.ini`;
-  const pathToResult1 = `${pathToTestDir}diff2`;
-  const pathToResult2 = `${pathToTestDir}diff2_plain`;
-  const result1 = fs.readFileSync(pathToResult1, 'utf-8');
-  const result2 = fs.readFileSync(pathToResult2, 'utf-8');
+  const pathToFile1 = path.join(fixturesPath, 'before2.ini');
+  const pathToFile2 = path.join(fixturesPath, 'after2.ini');
   it('#main format', () => {
     const diff = `${genDiff(pathToFile1, pathToFile2)}\n`;
-    expect(diff).toBe(result1);
+    expect(diff).toBe(prettyTreeExpected);
   });
   it('#plain format', () => {
     const diff = genDiff(pathToFile1, pathToFile2, 'plain');
-    expect(diff).toBe(result2);
+    expect(diff).toBe(plainTreeExpected);
   });
 });
